a7b19bbe72441857952004a934e0d4ff79ae84f5
AsandiAlwis <asandi.alwis@gmail.com>
2025-07-21 19:15:00 +0530

    slider changes

---
+++ routes/sliderRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  saveSlider,
+  getAllSliders,
+  getActiveSliders,
+  updateSlider,
+  deleteSlider,
+  getSliderImage,
+  upload
+} from "../controllers/sliderController.js";
+
+const router = express.Router();
+
+router.post("/save-slider", upload.single("image"), saveSlider);
+router.get("/all-sliders", getAllSliders);
+router.get("/active-sliders", getActiveSliders);
+router.get("/image/:id", getSliderImage);
+router.put("/update-slider/:id", upload.single("image"), updateSlider);
+router.delete("/delete-slider/:id", deleteSlider);
+
+export default router;
