acd533acbcb099b9a9b05b3610bb284db6ec4208
Paradoxrc <dinithminindu02@gmail.com>
2025-07-23 17:04:45 +0530

    Update contract address in configuration file

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ const config = {
   mongodbUri: process.env.MONGODB_URI,
   port: process.env.PORT || 5000,
   blockchainProviderUrl: process.env.BLOCKCHAIN_PROVIDER_URL,
-  contractAddress: "0x141C130E983Abda14090B771d251E06E18Df55eD", // Keep contract address here if it's config
+  contractAddress: "0x19e71E04BeE9722a5023e9ce40DcB1923187b95E", // Keep contract address here if it's config
   contractABI:[
     {
       "inputs": [],
